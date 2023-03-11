1e38e2d9558518fcddeaacd06b63bbc3abcd228a
Diego Alves <diegoteialves@gmail.com>
2023-03-11 15:20:45 -0300

    Calculadora

---
+++ calculadora.js
@@ -0,0 +1,19 @@
+function somar(numUm, numDois){
+    return numUm + numDois
+}
+
+function dividir(numUm, numDois){
+    return numUm / numDois
+}
+
+function subtrair(numUm, numDois){
+    return numUm + numDois
+}
+
+function multiplicar(numUm, numDois){
+    return numUm + numDois
+}
+
+module.exports = {somar, dividir, multiplicar, subtrair}
+
+
---
+++ index.js
@@ -0,0 +1,3 @@
+let calculadora = require("./calculadora.js")
+
+console.log(calculadora.somar(1, 2))
